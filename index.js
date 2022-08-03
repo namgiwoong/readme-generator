7050e5e2d086c4929237986982822e395abc11af
name <nameric@ymail.com>
2022-08-02 18:23:16 -0700

    reintalled npm file.

---
+++ index.js
@@ -0,0 +1,15 @@
+// require `inquirer`
+const inquirer = require("inquirer");
+const generateReadme = require("");
+
+//Create an array of question for user input
+const question = [];
+
+//Create a function to write README file
+function writeToFile(fileName, data) {}
+
+//Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
